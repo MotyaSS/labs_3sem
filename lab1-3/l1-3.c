c3f3cc4a7710172e304c7580b5f83f58e813737b
motya <motayzz32@gmail.com>
2023-09-24 17:54:44 +0300

    l1-3

--- lab1-3/l1-3.c
+++ lab1-3/l1-3.c
@@ -1,16 +1,40 @@
+#include <stdlib.h>
 #include <string.h>
 #include "l1-3.h"
 #include "../my_flag_lib.h"
 
 #define BUFF_SIZE 100
 
+int q_fl(const int argc, const char* argv[]) {
+  if (argc != 6)
+    return ARGC_ERROR;
+  if (!if_lf(argv[2]) || !if_lf(argv[3]) || !if_lf(argv[4]) || !if_ll(argv[5]))
+    return ARGUMENT_IR;
+  double ans[6][3];
+  double first, second, third, epsilon;
+  epsilon = strtod(argv[2], NULL);
+  first = strtod(argv[3], NULL);
+  second = strtod(argv[4], NULL);
+  third = strtod(argv[5], NULL);
+  return quadratic_eq(ans, first, second, third, epsilon);;
+}
+
+int m_fl(const int argc, const char* argv[]) {
+  return 0;
+}
+
+int t_fl(const int argc, const char* argv[]) {
+  return 0;
+}
+
 int flags_handling(char* flag, int argc, char* argv[]) {
   if (strcmp(flag, "q")) {
-    if (argc != 6)
-      return ARGC_ERROR;
+    return q_fl(argc, argv);
   } else if (strcmp(flag, "m")) {
     if (argc != 4)
       return ARGC_ERROR;
+    if (!if_ll(argv[2]) || !if_ll(argv[3]))
+      return ARGUMENT_IR;
   } else if (strcmp(flag, "t")) {
     if (argc != 6)
       return ARGC_ERROR;
--- lab1-3/l1-3.h
+++ lab1-3/l1-3.h
@@ -1,11 +1,19 @@
+#include <stdbool.h>
 
 enum {
-  TOO_FEW_ARGS = -10,
+  TOO_FEW_ARGS = 1,
   FLAG_ERROR,
   FLAG_UNKNOWN,
   FLAG_TOO_LONG,
-  ARGC_ERROR
+  ARGC_ERROR,
+  ARGUMENT_IR
 };
 
 int flags_handling(char* flag, int argc, char* argv[]);
 int input(int argc, char* argv[]);
+
+bool multiplicity(int first, int second);
+int quadratic_eq(double ans[6][3], double first, double second, double third, double eps);
+int solve_quadr_eq(int* ans, double a, double b, double c, double eps);
+
+bool triangle(double first, double second, double third, double eps);
