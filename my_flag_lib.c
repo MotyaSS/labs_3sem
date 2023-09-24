ebe08ef241041257b9bd74235c083cca4b612fe1
motya <motayzz32@gmail.com>
2023-09-24 15:47:17 +0300

    libs

---
+++ my_flag_lib.c
@@ -0,0 +1,44 @@
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include "my_flag_lib.h"
+
+bool if_flag(const char arg[]) {
+  if (arg == NULL)
+    return false;
+  if (arg[0] != '-' && arg[0] != '/') {
+    return false;
+  }
+  return true;
+}
+
+bool if_ll(const char arg[]) {
+  char temp1[80];
+  char* temp2;
+  strcpy(temp1, arg);
+  temp2 = temp1;
+  int len = strlen(arg);
+  if (arg[0] == '-') {
+    temp2 = temp1 + 1;
+    len--;
+  }
+
+  long long number = 0;
+  for (int i = 0; i < len; i++) {
+    if (temp2[i] < '0' || temp2[i] > '9')
+      return false;
+    if (number > LONG_LONG_MAX / 10)
+      return false;
+    if (number == (LONG_LONG_MAX / 10) && (temp2[i] - '0') > (LONG_LONG_MAX % 10))
+      return false;
+    number *= 10;
+    number += temp2[i] - '0';
+  }
+  return true;
+}
+
+void strcpy_without_first(const char cpy[], char var[]) {
+  if (cpy == NULL)
+    return;
+  strcpy(var, (cpy + 1));
+}
---
+++ my_flag_lib.h
@@ -0,0 +1,6 @@
+#include <stdbool.h>
+
+void strcpy_without_first(const char cpy[], char var[]);
+
+bool if_flag(const char arg[]);
+bool if_ll(const char arg[]);
