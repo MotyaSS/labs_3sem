b096656ba01a62f2e32ba99dd8761e3952f19b35
motya <motayzz32@gmail.com>
2023-09-24 16:22:42 +0300

    l1-3

--- lab1-3/l1-3.c
+++ lab1-3/l1-3.c
@@ -3,7 +3,17 @@
 #include "../my_flag_lib.h"
 #define BUFF_SIZE 100
 
-int flags(char* flag) {
+int flags(char* flag, int argc, char* argv[]) {
+  if(strcmp(flag, "q")){
+
+  } else if(strcmp(flag, "m")){
+
+  } else if(strcmp(flag, "t")){
+
+  }
+  else{
+    return FLAG_UNKNOWN;
+  }
   return 0;
 }
 
@@ -17,6 +27,5 @@ int input(int argc, char* argv[]) {
   if(strlen(argv[1]) >= BUFF_SIZE)
     return FLAG_TOO_LONG;
   strcpy_without_first(argv[1], flag);
-
-  return 0;
+  return flags(flag);
 }
--- lab1-3/l1-3.h
+++ lab1-3/l1-3.h
@@ -2,8 +2,9 @@
 enum {
   TOO_FEW_ARGS = -10,
   FLAG_ERROR,
+  FLAG_UNKNOWN,
   FLAG_TOO_LONG
 };
 
-int flags();
+int flags(char* flag, int argc, char* argv[]);
 int input(int argc, char* argv[]);
