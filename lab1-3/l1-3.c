c41044dc34c78640e2ad6fce22279ebb59db4446
motya <motayzz32@gmail.com>
2023-09-24 18:36:09 +0300

    l1-3

--- lab1-3/l1-3.c
+++ lab1-3/l1-3.c
@@ -10,38 +10,50 @@
 int q_fl_print(const int argc, const char* argv[]) {
   if (argc != 6)
     return ARGC_ERROR;
-  if (!if_lf(argv[2]) || !if_lf(argv[3]) || !if_lf(argv[4]) || !if_ll(argv[5]))
+  if (!if_lf(argv[2]) || !if_lf(argv[3]) || !if_lf(argv[4]) || !if_lf(argv[5]))
     return ARGUMENT_IR;
-  double ans[6][3];
+  double ans[6][2];
+  int ans_errs[6];
   double first, second, third, epsilon;
   epsilon = strtod(argv[2], NULL);
   first = strtod(argv[3], NULL);
   second = strtod(argv[4], NULL);
   third = strtod(argv[5], NULL);
-  int code = quadratic_eq(ans, first, second, third, epsilon);;
-  if (code == 0) {
-    //some stuff
+  int code = quadratic_eq(ans, ans_errs, first, second, third, epsilon);;
+  for (int i = 0; i < 6; i++) {
+    if (ans_errs[i] == -1) {
+      continue;
+    }
+    if (ans_errs[i] == 0) {
+      printf("%lf %lf\n", ans[i][0], ans[i][1]);
+    }
   }
   return code;
 }
 
-int quadratic_eq(double ans[6][2], double first, double second, double third, double eps) {
-  solve_quadr_eq(ans[0], first, second, third, eps);
-  solve_quadr_eq(ans[1], first, third, second, eps);
-  solve_quadr_eq(ans[2], second, third, first, eps);
-  solve_quadr_eq(ans[3], second, first, third, eps);
-  solve_quadr_eq(ans[4], third, first, second, eps);
-  solve_quadr_eq(ans[5], third, second, first, eps);
+int quadratic_eq(double ans[6][2], int ans_errs[6], double first, double second, double third, double eps) {
+  ans_errs[0] = solve_quadr_eq(ans[0], first, second, third, eps);
+  ans_errs[1] = solve_quadr_eq(ans[1], first, third, second, eps);
+  ans_errs[2] = solve_quadr_eq(ans[2], second, third, first, eps);
+  ans_errs[3] = solve_quadr_eq(ans[3], second, first, third, eps);
+  ans_errs[4] = solve_quadr_eq(ans[4], third, first, second, eps);
+  ans_errs[5] = solve_quadr_eq(ans[5], third, second, first, eps);
   return 0;
-
 }
 
 int solve_quadr_eq(double ans[2], double a, double b, double c, double eps) {
   double discr = b * b - 4 * a * c;
-  if(fabs(discr) < eps){
-    ans[0];
-    ans[1];
+  if (fabs(discr) < eps) {
+    ans[0] = (-b + sqrt(discr)) / (2 * a);
+    ans[1] = ans[0];
+    return 0;
   }
+  if (discr < 0) {
+    return -1;
+  }
+  ans[0] = (-b + sqrt(discr)) / (2 * a);
+  ans[1] = (-b - sqrt(discr)) / (2 * a);
+  return 0;
 }
 
 int m_fl_print(const int argc, const char* argv[]) {
@@ -57,16 +69,24 @@ int m_fl_print(const int argc, const char* argv[]) {
   return 0;
 }
 
+bool multiplicity(int first, int second) {
+
+}
+
 int t_fl_print(const int argc, const char* argv[]) {
   return 0;
 }
 
+bool triangle(double first, double second, double third, double eps) {
+
+}
+
 int flags_handling(char* flag, int argc, char* argv[]) {
-  if (strcmp(flag, "q")) {
+  if (strcmp(flag, "q") == 0) {
     return q_fl_print(argc, argv);
-  } else if (strcmp(flag, "m")) {
+  } else if (strcmp(flag, "m") == 0) {
     return m_fl_print(argc, argv);
-  } else if (strcmp(flag, "t")) {
+  } else if (strcmp(flag, "t") == 0) {
     if (argc != 6)
       return ARGC_ERROR;
   } else {
--- lab1-3/l1-3.h
+++ lab1-3/l1-3.h
@@ -17,7 +17,7 @@ int flags_handling(char* flag, int argc, char* argv[]);
 int input(int argc, char* argv[]);
 
 bool multiplicity(int first, int second);
-int quadratic_eq(double ans[6][2], double first, double second, double third, double eps);
+int quadratic_eq(double ans[6][2], int ans_errs[6], double first, double second, double third, double eps);
 int solve_quadr_eq(double ans[2], double a, double b, double c, double eps);
 
 bool triangle(double first, double second, double third, double eps);
