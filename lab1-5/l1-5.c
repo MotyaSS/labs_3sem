3a1ae9cc4334ceb8dd9485206335f48a9b0d937a
motya <motayzz32@gmail.com>
2023-09-27 16:42:40 +0300

    added sum_d + fixed prev sums

--- lab1-5/l1-5.c
+++ lab1-5/l1-5.c
@@ -26,39 +26,55 @@ int input(int argc, char* argv[]) {
 double sum_a(long x, double eps) {
   long n = 1;
   double cur = x;
+  double sum = cur;
   double prev;
   do {
     n++;
     prev = cur;
     cur = prev * x / n;
-  } while (fabs(cur - prev) > eps);
-  return cur;
+    sum += cur;
+  } while (fabs(cur) > eps);
+  return sum;
 }
 
 double sum_b(long x, double eps) {
   long n = 1;
   double cur = -1.0 * (x * x) / 2;
+  double sum = cur;
   double prev;
   do {
     prev = cur;
     cur = -1 * prev * x / (2 * n + 1) * x / (2 * n + 2);
+    sum += cur;
     n++;
-  } while (fabs(cur - prev) > eps);
-  return cur;
+  } while (fabs(cur) > eps);
+  return sum;
 }
 
 double sum_c(long x, double eps) {
   long n = 1;
   double cur = pow(3, 3) * x * x / (3 * 2);
+  double sum = cur;
   double prev;
   do {
     prev = cur;
     cur = prev * pow(3, 3) / (3 * n + 1) * pow(n + 1, 3) / (3 * n + 2) * x * x / (3 * n + 3);
+    sum += cur;
     n++;
-  } while (fabs(cur - prev) > eps);
-  return cur;
+  } while (fabs(cur) > eps);
+  return sum;
 }
 
 double sum_d(long x, double eps) {
-
+  long n = 1;
+  double cur = -1.0 * 1 * pow(x, 2) / 2;
+  double sum = cur;
+  double prev;
+  do {
+    prev = cur;
+    cur = -1.0 * prev * (2 * n + 1) / (2 * n + 2) * pow(x, 2);
+    sum += cur;
+    n++;
+  } while (fabs(cur) > eps);
+  return sum;
 }
