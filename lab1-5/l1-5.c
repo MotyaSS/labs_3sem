7ff3d30896aa2868e161a281fe78fea540e0d10b
motya <motayzz32@gmail.com>
2023-09-27 14:19:58 +0300

    -

--- lab1-5/l1-5.c
+++ lab1-5/l1-5.c
@@ -2,7 +2,9 @@
 #include "..\my_flag_lib.h"
 
 int input(int argc, char* argv[]) {
-  if (argc != 3)
+  if (argc != 2)
     return ARGC_ERROR;
+  if(!if_lf(argv[1]))
+    return ARG_NOT_LF;
   return 0;
 }
--- lab1-5/l1-5.h
+++ lab1-5/l1-5.h
@@ -1,4 +1,5 @@
 enum {
-  ARGC_ERROR = 1
+  ARGC_ERROR = 1,
+  ARG_NOT_LF
 };
 int input(int argc, char* argv[]);
