2bd5687a47c9678a124b651513762b81384dc3f9
motya <motayzz32@gmail.com>
2023-10-06 01:09:39 +0300

    lab rdy

--- lab1-8/main.c
+++ lab1-8/main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include "l1-8.h"
 
-int main() {
-  printf("Hello, World!\n");
+int main(int argc, char* argv[]) {
+  switch (input(argc, argv)) {
+    case OK:
+      printf("All ok\n");
+      break;
+    case INVALID_FILE:
+      printf("File unknown\n");
+      break;
+    case INVALID_ARGC:
+      printf("Invalid argument count\n");
+      break;
+    default:
+      printf("Unknown status code\n");
+  }
+
   return 0;
 }
