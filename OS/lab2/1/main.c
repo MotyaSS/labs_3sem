d1e595724ce87263cdb39d40b7bc873c330f3e62
MotyaSS <motyazz32@gmail.com>
2023-10-23 01:17:29 +0300

    -

---
+++ OS/lab2/1/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+  printf("%d %d %d %d %d %d",
+         getpid(),  //cur proc id
+         getppid(), //parent proc id
+         getgid(),  //real owner group id
+         getuid(),  //real owner id
+         getegid(), //eff owner group id
+         geteuid()  //eff owner id
+  );
+}
