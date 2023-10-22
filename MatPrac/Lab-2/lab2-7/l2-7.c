6c8b3815023518f443d05507e586d28fcf38a55b
motya <motayzz32@gmail.com>
2023-10-22 21:10:33 +0300

    lab done

---
+++ MatPrac/Lab-2/lab2-7/l2-7.c
@@ -0,0 +1,27 @@
+#include "l2-7.h"
+#include <math.h>
+
+int dichotomy(double* res, double left_bound, double right_bound, double eps, double (*func)(double)) {
+  if (left_bound * right_bound > 0) {
+    *res = 0;
+    return 1;
+  }
+  if(eps <= 0){
+    return 2;
+  }
+  double mid;
+  double f_mid;
+  do {
+    mid = (left_bound + right_bound) / 2;
+    f_mid = func(mid);
+    if (f_mid * (func(left_bound)) > 0) {
+      left_bound = mid;
+    }
+    if (f_mid * (func(right_bound)) > 0) {
+      right_bound = mid;
+    }
+  } while (fabs(f_mid) > eps);
+  *res = mid;
+
+  return 0;
+}
--- MatPrac/Lab-2/lab2-7/main.c
+++ MatPrac/Lab-2/lab2-7/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-
+#include "l2-7.h"
+#include "math.h"
 int main() {
-  printf("Hello, World!\n");
+  double res;
+  dichotomy(&res, 0, 3.14, 0.1, cos);
+  printf("%lf", res);
   return 0;
 }
