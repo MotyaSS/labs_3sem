5e5f3737287af7e3c5b5186f3bb3fdc33b15928b
MotyaSS <motyazz32@gmail.com>
2023-10-29 18:25:43 +0300

    -

--- OS/lab2/1/main.c
+++ OS/lab2/1/main.c
@@ -3,12 +3,12 @@
 
 int main() {
   printf("%d %d %d %d %d %d %d",
-         getpid(),  //cur proc id
-         getppid(), //parent proc id
-         getpgrp(), //process group id
-         getgid(),  //real owner group id
-         getuid(),  //real owner id
-         getegid(), //eff owner group id
-         geteuid()  //eff owner id
+         getpid(),    //cur proc id
+         getppid(),   //parent proc id
+         getpgrp(),   //process group id
+         getgid(),    //real owner group id
+         getuid(),    //real owner id
+         getegid(),   //eff owner group id
+         geteuid()    //eff owner id
   );
 }
