9dc81335ecbcd72a55d034ce6c0c57526d438507
motya <motayzz32@gmail.com>
2023-10-29 22:23:07 +0300

    small fix

--- MatPrac/Lab-2/lab2-3/main.c
+++ MatPrac/Lab-2/lab2-3/main.c
@@ -2,12 +2,12 @@
 #include "l2-3.h"
 #include <malloc.h>
 
-void erase(char_info** info) {
+void erase(char_info** info, size_t cnt) {
   char_info** ptr = info;
-  do {
+  for (int i = 0; i < cnt; i++) {
     free(*ptr);
     ptr++;
-  } while (*ptr != NULL);
+  }
   free(info);
 }
 
@@ -15,7 +15,7 @@ void print(char_info* const* info, size_t cnt) {
   const char_info* const* info_p = info;
   for (int i = 0; i < cnt; i++) {
     char_info const* ptr = *info_p;
-    if(ptr == NULL) {
+    if (ptr == NULL) {
       continue;
     }
 
@@ -28,13 +28,17 @@ void print(char_info* const* info, size_t cnt) {
   }
 }
 
+void print_erase(char_info** info, size_t cnt) {
+  print(info, cnt);
+  erase(info, cnt);
+}
+
 int main() {
   char_info** indexes_arr;
-  if (find_all_substr_in_multiple_files(&indexes_arr, 2, "sv", "test.txt", "test2.txt") != find_ok) {
+  if (find_all_substr_in_multiple_files(&indexes_arr, 3, "sv", "test.txt", "../test.txt", "test2.txt") != find_ok) {
     printf("something went wrong\n");
   } else {
-    print(indexes_arr, 2);
-    erase(indexes_arr);
+    print_erase(indexes_arr, 3);
     indexes_arr = NULL;
   }
   return 0;
