9ec30df953377afb99790679e029cc7d798778d9
motya <motayzz32@gmail.com>
2023-11-02 00:32:48 +0300

    some added

--- my_flag_lib.c
+++ my_flag_lib.c
@@ -86,6 +86,33 @@ bool if_i(const char arg[]) {
   return true;
 }
 
+bool if_u(const char arg[]) {
+  if (arg == NULL) {
+    return false;
+  }
+  char temp1[80];
+
+  char* temp2;
+  strcpy(temp1, arg);
+  temp2 = temp1;
+
+  int number = 0;
+  for (int i = 0; arg[i] != 0; i++) {
+    if (temp2[i] < '0' || temp2[i] > '9') {
+      return false;
+    }
+    if (number > UINT_MAX / 10) {
+      return false;
+    }
+    if (number == (UINT_MAX / 10) && (temp2[i] - '0') > (UINT_MAX % 10)) {
+      return false;
+    }
+    number *= 10;
+    number += temp2[i] - '0';
+  }
+  return true;
+}
+
 bool if_ll(const char arg[]) {
   if (arg == NULL) {
     return false;
@@ -143,7 +170,7 @@ void strcpy_without_first(const char cpy[], char var[]) {
 #include "stdio.h"
 
 int close_files(int count, ...) {
-  if(count <= 0) {
+  if (count <= 0) {
     return -1;
   }
   va_list ap;
@@ -155,5 +182,37 @@ int close_files(int count, ...) {
     }
   }
   va_end(ap);
+  return 0;
+}
+
+
+int my_fgets(FILE* stream, char* buff, unsigned int cap) {
+
+  if (stream == NULL) {
+    return -1;
+  }
+  if (buff == NULL) {
+    return -2;
+  }
+
+  int ch;
+  while (isspace(ch = fgetc(stream)));
+
+  if (feof(stream)) {
+    return 1;
+  }
+
+  int cnt = 0;
+  buff[cnt++] = ch;
+  while ((ch = fgetc(stream)) != EOF && !isspace(ch) && cnt < cap - 1) {
+    buff[cnt++] = ch;
+  }
+  buff[cnt] = '\0';
+
+  //skip rest letters of current lexema
+  while ((ch = fgetc(stream)) != EOF && !isspace(ch));
+  if(ch != EOF){
+    fseek(stream, -1, SEEK_CUR);
+  }
   return 0;
 }
--- my_flag_lib.h
+++ my_flag_lib.h
@@ -2,6 +2,7 @@
 #define MY_FL_L
 
 #include <stdbool.h>
+#include <stdio.h>
 
 void strcpy_without_first(const char cpy[], char var[]);
 
@@ -10,6 +11,9 @@ bool if_ll(const char arg[]);
 bool if_i(const char arg[]);
 bool if_l(const char arg[]);
 bool if_lf(const char arg[]);
+bool if_u(const char arg[]);
+
+int my_fgets(FILE* stream, char* buff, unsigned int cap);
 
 int close_files(int count, ...);
 #endif
