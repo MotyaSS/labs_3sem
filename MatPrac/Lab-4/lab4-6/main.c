ea73de9b4a9e04e96b1f20cdf40d3736a82418f3
MotyaSS <82867670+motyass@users.noreply.github.com>
2023-12-20 12:29:04 +0300

    .

--- MatPrac/Lab-4/lab4-6/main.c
+++ MatPrac/Lab-4/lab4-6/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "l4-6.h"
+#include "../../Lab-3/lab3-4/my_string.h"
 
 
 int print_tree(TNode* node, int level) {
@@ -15,20 +16,43 @@ int print_tree(TNode* node, int level) {
     return 0;
 }
 
-int main() {
-    Tree tree;
-    int code;
-    char* expr = "~a -> b&1";
-    if ((code = build_tree(expr, &tree)) != OK) {
-        printf("Err code:%d\n", code);
+int execute(int argc, char** argv) {
+    if (argc != 2) {
         return 1;
     }
+
+    FILE* in = fopen(argv[1], "r");
+    if (!in) {
+        return 2;
+    }
+    String a;
+    if (string_init(&a, 16) != 0) {
+        fclose(in);
+        return 3;
+    }
+
+    if (getline(&a, in) != get_str_ok) {
+        string_destr(&a);
+        fclose(in);
+        return 3;
+    }
+    fclose(in);
+
+    Tree tree;
+    int code;
+    if ((code = build_tree(a._buf, &tree)) != OK) {
+        string_destr(&a);
+        printf("Err code: %d\n", code);
+        return 4;
+    }
+
 
     char filename[17];
     get_rand_name(filename, 16);
     FILE* out = fopen(filename, "w");
     if (!out) {
         printf("Unable to generate file\n");
+        string_destr(&a);
         free(tree.vars.arr);
         tnode_destr(tree.root);
     }
@@ -36,11 +60,19 @@ int main() {
 
 
     print_tree(tree.root, 0);
-    fprintf(out, "Expression is :\n %s\n", expr);
+    fprintf(out, "Expression is :\n %s\n", a._buf);
+    string_destr(&a);
     print_table(out, &tree);
-
     fclose(out);
+
+
     free(tree.vars.arr);
     tnode_destr(tree.root);
     return 0;
+}
+
+int main(int argc, char* argv[]) {
+    if(execute(argc, argv)){
+        printf("something went wrong");
+    }
 }
